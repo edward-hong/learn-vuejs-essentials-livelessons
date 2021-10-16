cbc21a3bc216b73395ba3b854cfdd3ca761cf200
Edward Hong <edward.hong527@gmail.com>
2021-10-16 15:27:59 +1300

    S01L04 Understand the Different Parts of a Vue Component

--- my-vue-project/src/App.vue
+++ my-vue-project/src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Everyone!!" />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Hello Everyone!!" />
+  </div>
 </template>
 
 <script>
