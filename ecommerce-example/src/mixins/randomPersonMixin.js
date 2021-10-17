13af7ffb7c1b394ec4099be5c88f178fe441c9b3
Edward Hong <edward.hong527@gmail.com>
2021-10-17 23:41:06 +1300

    S05L01 Learn About Vue Mixins

---
+++ ecommerce-example/src/mixins/randomPersonMixin.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      personData: null,
+    }
+  },
+  created() {
+    axios
+      .get('https://randomuser.me/api/')
+      .then(response => (this.personData = response.data.results[0]))
+  },
+}
--- ecommerce-example/src/pages/EmployeeOfTheMonthPage.vue
+++ ecommerce-example/src/pages/EmployeeOfTheMonthPage.vue
@@ -8,19 +8,10 @@
 </template>
 
 <script>
-  import axios from 'axios'
+  import randomPersonMixin from '../mixins/randomPersonMixin'
 
   export default {
     name: 'EmployeeOfTheMonthPage',
-    data() {
-      return {
-        personData: null,
-      }
-    },
-    created() {
-      axios
-        .get('https://randomuser.me/api/')
-        .then(response => (this.personData = response.data.results[0]))
-    },
+    mixins: [randomPersonMixin],
   }
 </script>
