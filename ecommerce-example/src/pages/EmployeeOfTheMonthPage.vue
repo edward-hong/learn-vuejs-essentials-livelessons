a321f4b21076f3baa5786c5c8be64c3e25e33a5f
Edward Hong <edward.hong527@gmail.com>
2021-10-17 23:34:07 +1300

    S04L03 Load Data and Make Requests in Vue

---
+++ ecommerce-example/src/pages/EmployeeOfTheMonthPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Employee of the Month</h1>
+    <div v-if="personData">
+      <h3>{{ personData.name.first }} {{ personData.name.last }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'EmployeeOfTheMonthPage',
+    data() {
+      return {
+        personData: null,
+      }
+    },
+    created() {
+      axios
+        .get('https://randomuser.me/api/')
+        .then(response => (this.personData = response.data.results[0]))
+    },
+  }
+</script>
