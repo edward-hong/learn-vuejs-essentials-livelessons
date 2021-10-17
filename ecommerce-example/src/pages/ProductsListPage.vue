70da06beb9cc96f59ab98dc25385eb696cf41119
Edward Hong <edward.hong527@gmail.com>
2021-10-17 16:16:40 +1300

    S03L01 Set Up Routing in Vue

---
+++ ecommerce-example/src/pages/ProductsListPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>Available Items:</h2>
+    <div v-for="product in products" :key="product.id">
+      <router-link :to="`/products/${product.id}`">
+        <h3>{{ product.name }}</h3>
+      </router-link>
+      <p>{{ product.price }}</p>
+      <button>Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import products from './products'
+
+  export default {
+    name: 'ProductsListPage',
+    data() {
+      return { products }
+    },
+  }
+</script>
